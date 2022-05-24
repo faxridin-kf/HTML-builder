65c7dac4dfead7a01b5dee284b8413ef5b90801c
Fakhridin <66880870+faxridin-kf@users.noreply.github.com>
2022-05-24 18:41:13 +0500

    fix: file name

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathToFile = path.join(__dirname, '1.txt');
+const pathToFile = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(pathToFile, 'utf-8');
 
 readStream.on('data', (chunk) => {
